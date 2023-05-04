e7a4feff29806e59902c438009615c500cb70b4b
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-05 01:00:55 +0300

    refactor the header into own module

---
+++ app/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+function Header(props) {
+  return (
+    <View style={styles.header}>
+      <View>
+        <Text style={styles.title}>My model S</Text>
+        <Text style={styles.subtitle}>Parked</Text>
+      </View>
+      <View>
+        <FontAwesome name="user-circle" size={30} color="gray" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#eee",
+    marginBottom: 5,
+  },
+  subtitle: {
+    color: "gray",
+    fontWeight: "500",
+  },
+});
+
+export default Header;
--- app/index.js
+++ app/index.js
@@ -1,35 +1,18 @@
-import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import { StyleSheet, View, Image, FlatList } from "react-native";
 import Constants from "expo-constants";
-import {
-  FontAwesome,
-  Entypo,
-  MaterialCommunityIcons,
-  FontAwesome5,
-  Ionicons,
-} from "@expo/vector-icons";
 
 import car from "../assets/images/car.png";
 import menuOptions from "../assets/menuOptions";
 import MenuOption from "./components/MenuOption";
 import ControlRow from "./components/ControlRow";
+import Header from "./components/Header";
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View>
-          <Text style={styles.title}>My model S</Text>
-          <Text style={styles.subtitle}>Parked</Text>
-        </View>
-        <View>
-          <FontAwesome name="user-circle" size={30} color="gray" />
-        </View>
-      </View>
-
+      <Header />
       <Image style={styles.image} source={car} />
-
       <ControlRow />
-
       <FlatList
         showsVerticalScrollIndicator={false}
         data={menuOptions}
@@ -47,23 +30,9 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
   },
 
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
   image: {
     width: "100%",
     height: 200,
     resizeMode: "contain",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#eee",
-    marginBottom: 5,
-  },
-  subtitle: {
-    color: "gray",
-    fontWeight: "500",
   },
 });
